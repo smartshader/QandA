5e0df969e70861b5351a73029bf0944cc78b44d0
Imad Jerra <smartshader@gmail.com>
2020-08-29 15:26:51 -0400

    Styling components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
 import { Header } from './components/Header';
 import { Home } from './components/Home';
+import { fontFamily, fontSize, gray2 } from './Styles';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <Home />
     </div>
   );
-}
+};
 
 export default App;
